6ac539615f4589c46b59b06c709d154649ad36c9
Anant-001 <anantdevliyal01@gmail.com>
2025-07-24 11:39:21 +0530

    Initial commit

---
+++ src/3D/Animated modal.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const AnimatedModal = () => {
+    return (
+        <div className="w-full max-w-[600px] aspect-video rounded-xl overflow-hidden shadow-xl">
+            <iframe
+                title="Jibril - NoGameNoLife (rigged)"
+                src="https://sketchfab.com/models/ac2a1ce175104faa8469675b4726e2a5/embed?autostart=1&preload=1&ui_hint=2"
+                frameBorder="0"
+                allow="autoplay; fullscreen; xr-spatial-tracking"
+                allowFullScreen
+                mozAllowFullScreen="false"
+                webkitAllowFullScreen="False"
+                className="w-full h-full"
+            ></iframe>
+        </div>
+    )
+}
+export default AnimatedModal
---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import Sidebar from "./components/Sidebar";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Contect from "./pages/Contect.jsx";
+import Skills from "./pages/Skills.jsx";
+import Footer from "./pages/Footer.jsx";
+
+function App() {
+    return (
+        <div className="min-h-screen bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900 text-white md:pl-56">
+            <Sidebar />
+            <main className="p-4 space-y-32">
+                <Home />
+                <About />
+                <Projects />
+                <Skills />
+                <Contect />
+                <Footer />
+            </main>
+        </div>
+    );
+}
+
+export default App;
